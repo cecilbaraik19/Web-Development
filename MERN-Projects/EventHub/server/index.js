47cf512ed2b7241fbd23aa7668c7b8497b9f13b5
CecilBaraik <cecilbaraik19@gmail.com>
2026-07-12 08:38:28 +0530

    Setting up backened API Routes and Controller

--- MERN-Projects/EventHub/server/index.js
+++ MERN-Projects/EventHub/server/index.js
@@ -7,12 +7,21 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/auth')
+const authRoutes = require('./routes/auth');
+const eventRoutes = require('./routes/events');
+const bookingRoutes = require('./routes/booking');
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use('./api/auth',authRoutes);
+app.use('/api/events',eventRoutes);
+app.use('/api/bookings',bookingRoutes);
+
 
 //Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
