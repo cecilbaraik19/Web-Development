cd0e446dacfdbb693fbf60fb6697159dfb2a0446
CecilBaraik <cecilbaraik19@gmail.com>
2026-07-14 23:06:11 +0530

    Frontent setup and installation of Tailwind CSS

--- MERN-Projects/EventHub/server/models/OTP.js
+++ MERN-Projects/EventHub/server/models/OTP.js
@@ -8,6 +8,16 @@ const otpSchema = new mongoose.Schema({
     otp:{
         type:String,
         required:true
+    },
+    action:{
+        type: String,
+        enum: ['account_verification','event_booking'],
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now,
+        expires: 300 // OTP expires after 5 minutes
     }
 });
 module.exports = mongoose.model('OTP',otpSchema);
