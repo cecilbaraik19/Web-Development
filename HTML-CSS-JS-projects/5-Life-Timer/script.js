28a58f7b006a852337fe0f061b4a075c377fcce5
CecilBaraik <cecilbaraik19@gmail.com>
2026-07-26 21:51:11 +0530

    new line of code added

--- HTML-CSS-JS-projects/5-Life-Timer/script.js
+++ HTML-CSS-JS-projects/5-Life-Timer/script.js
@@ -38,6 +38,53 @@ const updateAge = () => {
   const hour = Math.floor(dateDiff / (1000 * 60 * 60)) % 24;
   const minute = Math.floor(dateDiff / (1000 * 60)) % 60;
   const second = Math.floor(dateDiff / 1000) % 60;
-  
+
+  yearEl.innerHTML = makeTwoDigitNumber(year);
+  monthEl.innerHTML = makeTwoDigitNumber(month);
+  dayEl.innerHTML = makeTwoDigitNumber(day);
+  hourEl.innerHTML = makeTwoDigitNumber(hour);
+  minuteEl.innerHTML = makeTwoDigitNumber(minute);
+  secondEl.innerHTML = makeTwoDigitNumber(second);
+};
+
+const localStorageGetter = () => {
+  const year = localStorage.getItem("year");
+  const month = localStorage.getItem("month");
+  const date = localStorage.getItem("date");
+  if (year && month && date) {
+    dateOfBirth = new Date(year, month, date);
+  }
+
+  updateAge();
+};
+
+const contentToggler = () => {
+  updateAge();
+  if (dateOfBirth) {
+    initialTextEl.classList.add("hide");
+    afterDOBBtnTxtEl.classList.remove("hide");
+  } else {
+    afterDOBBtnTxtEl.classList.add("hide");
+    initialTextEl.classList.remove("hide");
+  }
+};
+const setDOBHandler = () => {
+  const dateString = dobInputEl.value;
+
+  dateOfBirth = dateString ? new Date(dateString) : null;
+
+  if (dateOfBirth) {
+    localStorage.setItem("year", dateOfBirth.getFullYear());
+    localStorage.setItem("month", dateOfBirth.getMonth());
+    localStorage.setItem("date", dateOfBirth.getDate());
+  }
+
+  contentToggler();
+  setInterval(() => updateAge(), 1000);
+};
+
+localStorageGetter();
+contentToggler();
+
 settingCoEl.addEventListener("click",toggleDateofBirthSelector);
-dobButtonEl.addEventListener("click",toggleDateofBirthSelector);
+dobButtonEl.addEventListener("click",setDOBHandler);
