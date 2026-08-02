94a470bc60815ba35edd34ff5572e0b1d8417017
CecilBaraik <cecilbaraik19@gmail.com>
2026-08-02 13:19:34 +0530

    installation of tailwind css done

--- React-Tailwindcss-Projects/swiggy-clone/src/App.jsx
+++ React-Tailwindcss-Projects/swiggy-clone/src/App.jsx
@@ -1,8 +1,12 @@
-function App(){
-    return (
-        <>
-        </>
-    );
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-orange-500">
+        Swiggy Clone
+      </h1>
+    </div>
+  );
 }
 
 export default App;
