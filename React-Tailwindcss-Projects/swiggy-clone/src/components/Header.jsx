ce4df1bb828906ea21ce0ae01c2858a72600ff5d
CecilBaraik <cecilbaraik19@gmail.com>
2026-08-04 00:22:32 +0530

    Header Section Compeleted

--- React-Tailwindcss-Projects/swiggy-clone/src/components/Header.jsx
+++ React-Tailwindcss-Projects/swiggy-clone/src/components/Header.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react'
 import { RxCaretDown } from "react-icons/rx";
+import { MdSearch } from "react-icons/md";
+import { RiDiscountPercentLine } from "react-icons/ri";
+import { IoMdHelpCircleOutline } from "react-icons/io";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
 export default function Header() {
     const [toggle, setToggle] = useState(false);
     const showSideMenu = () =>{
@@ -8,6 +13,29 @@ export default function Header() {
     const hideSideMenu = () =>{
         setToggle(false);
     }
+    const links = [
+        {
+            icon:<MdSearch/>,
+            name:"Search"
+        },
+        {
+            icon:<RiDiscountPercentLine />,
+            name:"Offers",
+            sup:"New"
+        },
+        {
+            icon:<IoMdHelpCircleOutline />,
+            name:"Help"
+        },
+        {
+            icon:<MdOutlineShoppingCart />,
+            name:"Cart"
+        },
+        {
+            icon:<CgProfile />,
+            name:"Sign In"
+        },
+    ]
   return (
     <>
     <div className='black-overlay w-full h-full fixed duration-500' onClick={hideSideMenu} style={{
@@ -26,6 +54,19 @@ export default function Header() {
                 <img className='h-[69px]' src="/images/logo.png" alt="" />
             </div>
             <div><span  className='font-bold border-b-[3px] border-[black]'>Ratu</span> Ranchi, Jharkhand, India <RxCaretDown fontSize={25} className='inline text-[#fc8019] cursor-pointer' onClick={showSideMenu}/></div>
+            <nav className='flex list-none gap-5 ml-auto text-[18px] font-semibold'>
+                {
+                    links.map(
+                        (link,index) => {
+                            return <li className='cursor-pointer flex hover:text-[#fc8019] items-center gap-2  duration-[400ms]'>
+                                {link.icon}
+                                {link.name}
+                                <sup>{link.sup}</sup>
+                            </li>
+                        }
+                    )
+                }
+            </nav>
         </div>
     </header>
     </>
