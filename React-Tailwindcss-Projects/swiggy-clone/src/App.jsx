0c6376574a39776f0d61abd678c5b1b1b5f77c5e
CecilBaraik <cecilbaraik19@gmail.com>
2026-08-03 22:57:44 +0530

    logo and side address bar menu done

--- React-Tailwindcss-Projects/swiggy-clone/src/App.jsx
+++ React-Tailwindcss-Projects/swiggy-clone/src/App.jsx
@@ -1,11 +1,9 @@
-
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-black">
-      <h1 className="text-5xl font-bold text-orange-500">
-        Swiggy Clone
-      </h1>
-    </div>
+    <>
+    <Header/>
+    </>
   );
 }
 
---
+++ React-Tailwindcss-Projects/swiggy-clone/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { RxCaretDown } from "react-icons/rx";
+export default function Header() {
+    const [toggle, setToggle] = useState(false);
+    const showSideMenu = () =>{
+        setToggle(true);
+    }
+    const hideSideMenu = () =>{
+        setToggle(false);
+    }
+  return (
+    <>
+    <div className='black-overlay w-full h-full fixed duration-500' onClick={hideSideMenu} style={{
+        opacity: toggle ? 1 : 0,
+        visibility: toggle ? "visible" : "hidden"
+    }}>
+        <div onClick={(e) => {e.stopPropagation()}} className='w-[500px] bg-white h-full absolute duration-[400ms]'
+             style={{
+                left: toggle ? '0%' : '-100%'
+            }}
+        ></div>
+    </div>
+    <header className='p-3 shadow-xl'>
+        <div className='max-w-[1200px] mx-auto flex items-center'>
+            <div className='w-[100px]'>
+                <img src="/images/logo.png" alt="" />
+            </div>
+            <div><span  className='font-bold border-b-[3px] border-[black]'>Ratu</span> Ranchi, Jharkhand, India <RxCaretDown fontSize={25} className='inline text-[#fc8019] cursor-pointer' onClick={showSideMenu}/></div>
+        </div>
+    </header>
+    </>
+  )
+}
