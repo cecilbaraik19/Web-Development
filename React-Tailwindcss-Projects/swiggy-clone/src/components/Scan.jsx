e1ad7bf53509891ff26e868c3a05759cbfd412ed
CecilBaraik <cecilbaraik19@gmail.com>
2026-08-07 00:18:09 +0530

    Scan QR part done

---
+++ React-Tailwindcss-Projects/swiggy-clone/src/components/Scan.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Scan() {
+  return (
+    <>
+    <div className="max-w-[1200px] mx-auto mt-20 mb-20">
+      <img
+        src="/images/scan.avif"
+        alt="Scan QR"
+        className="w-full rounded-2xl"
+      />
+    </div>
+    <hr className="my-6 border-[1px]" />
+    </>
+  );
+}
