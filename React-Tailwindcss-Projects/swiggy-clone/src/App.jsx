8cd15fdee76962c59e323221724adcf3a499fb2e
CecilBaraik <cecilbaraik19@gmail.com>
2026-08-07 00:02:03 +0530

    Top Restaurant part done with api

--- React-Tailwindcss-Projects/swiggy-clone/src/App.jsx
+++ React-Tailwindcss-Projects/swiggy-clone/src/App.jsx
@@ -1,10 +1,12 @@
 import Category from "./components/Category";
 import Header from "./components/Header";
+import TopRest from "./components/TopRest";
 function App() {
   return (
     <>
     <Header/>
     <Category/>
+    <TopRest/>
     </>
   );
 }
---
+++ React-Tailwindcss-Projects/swiggy-clone/src/api/restaurantData.js
@@ -0,0 +1,82 @@
+const topRestaurantData = [
+  {
+    id: 1,
+    image: "/images/restaurant/amora.webp",
+    name: "Cafe Amore",
+    cuisine: "Chinese • Continental",
+    location: "Singh More, Ranchi",
+    price: "₹500 for two",
+    distance: "5.3 km",
+    rating: "4.6",
+    offer: "Flat 10% off on walk-in",
+    extra: "+1 more",
+    bankOffer: "Up to 10% off with bank offers"
+  },
+  {
+    id: 2,
+    image: "/images/restaurant/planet.webp",
+    name: "Planet Cafe & Restro",
+    cuisine: "North Indian • Chinese",
+    location: "Doranda, Ranchi",
+    price: "₹400 for two",
+    distance: "2 km",
+    rating: "4.8",
+    offer: "Flat 10% off on walk-in",
+    extra: "+1 more",
+    bankOffer: "Up to 10% off with bank offers"
+  },
+  {
+    id: 3,
+    image: "/images/restaurant/blue.webp",
+    name: "Blue Ridge Sweets",
+    cuisine: "North Indian • Fast Food",
+    location: "Argora, Ranchi",
+    price: "₹500 for two",
+    distance: "3 km",
+    rating: "4.3",
+    offer: "Flat 10% off on pre-booking",
+    extra: "+1 more",
+    bankOffer: "Up to 10% off with bank offers"
+  },
+  {
+    id: 4,
+    image: "/images/restaurant/cilantro.webp",
+    name: "Cilantro Restaurant",
+    cuisine: "North Indian • Chinese",
+    location: "Doranda, Ranchi",
+    price: "₹700 for two",
+    distance: "1 km",
+    rating: "4.0",
+    offer: "Flat 10% off on walk-in",
+    extra: "+1 more",
+    bankOffer: "Up to 10% off with bank offers"
+  },
+  {
+    id: 5,
+    image: "/images/restaurant/campfire.webp",
+    name: "Barbeque Campfire",
+    cuisine: "North Indian • Chinese",
+    location: "Kadru, Ranchi",
+    price: "₹1000 for two",
+    distance: "2.6 km",
+    rating: "4.4",
+    offer: "Flat 10% off on walk-in",
+    extra: "+1 more",
+    bankOffer: "Up to 10% off with bank offers"
+  },
+  {
+    id: 6,
+    image: "/images/restaurant/bawarchi.webp",
+    name: "Bawarchi",
+    cuisine: "North Indian • Chinese",
+    location: "Kadru, Ranchi",
+    price: "₹1000 for two",
+    distance: "3 km",
+    rating: "3.9",
+    offer: "Flat 10% off on walk-in",
+    extra: "+1 more",
+    bankOffer: "Up to 10% off with bank offers"
+  }
+];
+
+export default topRestaurantData;
