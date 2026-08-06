ca6b2a50e6d27f0290cb0f159295d02800db8ea8
CecilBaraik <cecilbaraik19@gmail.com>
2026-08-06 12:22:26 +0530

    Category.jsx done with modifying in code and added categoryApi.js

---
+++ React-Tailwindcss-Projects/swiggy-clone/src/api/categoryApi.js
@@ -0,0 +1,104 @@
+const categories = [
+  {
+    id: 1,
+    name: "North Indian",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/North%20Indian.png"
+  },
+  {
+    id: 2,
+    name: "South Indian",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/South%20Indian.png"
+  },
+  {
+    id: 3,
+    name: "Dosa",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Dosa.png"
+  },
+  {
+    id: 4,
+    name: "Cake",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Cake.png"
+  },
+  {
+    id: 5,
+    name: "Desserts",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Desserts.png"
+  },
+  {
+    id: 6,
+    name: "Pizza",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Pizza.png"
+  },
+  {
+    id: 7,
+    name: "Biryani",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Biryani.png"
+  },
+  {
+    id: 8,
+    name: "Idli",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Idli.png"
+  },
+  {
+    id: 9,
+    name: "Chole Bhature",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Chole%20Bhature.png"
+  },
+  {
+    id: 10,
+    name: "Chinese",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Chinese.png"
+  },
+  {
+    id: 11,
+    name: "Paratha",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Paratha.png"
+  },
+  {
+    id: 12,
+    name: "Rolls",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Rolls.png"
+  },
+  {
+    id: 13,
+    name: "Jalebi",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Jalebi.png"
+  },
+  {
+    id: 14,
+    name: "Burger",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Burger.png"
+  },
+  {
+    id: 15,
+    name: "Gulab Jamun",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Gulab%20Jamun.png"
+  },
+  {
+    id: 16,
+    name: "Rasgulla",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Rasgulla.png"
+  },
+  {
+    id: 17,
+    name: "Poori",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Poori.png"
+  },
+  {
+    id: 18,
+    name: "Vada",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Vada.png"
+  },
+  {
+    id: 19,
+    name: "Noodles",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Noodles.png"
+  },
+  {
+    id: 20,
+    name: "Pastry",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Pastry.png"
+  }
+];
+
+module.exports = categories;
--- React-Tailwindcss-Projects/swiggy-clone/src/components/Category.jsx
+++ React-Tailwindcss-Projects/swiggy-clone/src/components/Category.jsx
@@ -1,55 +1,55 @@
-import React, { useEffectEvent, useState } from "react";
+import React, {useState, useEffect } from "react";
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import categoriesData from "../api/categoryApi.js";
 
-export default function () {
-    const [slide,setSlide] = useState(0);
-    const [categories, setCategory] = useState([]);
-    const fetchCategory = async () => {
-        const response = await fetch("http://localhost:5000/categories");
-        const data = await response.json();
-        setCategory(data);
-    }
-    useEffect(
-        () => {
-            fetchCategory();
-        },[]
-    )
-    const nextSlide = () =>{
-        if(categories.length - 8 == slide) return false;
-        setSlide(slide + 3);
-    }
-    const prevSlide = () =>{
-        if(slide == 0) return false;
-        setSlide(slide - 3);
-    }
+export default function Category () {
+  const [slide, setSlide] = useState(0);
+  const [categories, setCategory] = useState([]);
+
+  useEffect(() => {
+    setCategory(categoriesData);
+  }, []);
+
+  const nextSlide = () => {
+    if (categories.length - 8 === slide) return false;
+    setSlide(slide + 3);
+  };
+  const prevSlide = () => {
+    if (slide === 0) return false;
+    setSlide(slide - 3);
+  };
   return (
     <>
       <div className="max-w-[1200px] mx-auto">
         <div className="flex my-3 items-center justify-between">
           <div className="text-[25px] font-bold">What's on your mind?</div>
           <div className="flex">
-            <div className="flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 cursor-pointer" onClick={prevSlide}>
-            <FaArrowLeft />
-          </div>
-          <div className="flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 cursor-pointer" onClick={nextSlide}>
-            <FaArrowRight />
-          </div>
+            <div
+              className="flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 cursor-pointer"
+              onClick={prevSlide}
+            >
+              <FaArrowLeft />
+            </div>
+            <div
+              className="flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 cursor-pointer"
+              onClick={nextSlide}
+            >
+              <FaArrowRight />
+            </div>
           </div>
         </div>
         <div className="flex overflow-hidden">
-            {
-                categories.map(
-                    (cat,index) => {
-                        return(
-                            <div style={{
-                                transform:` translateX(-${slide * 100}%)`
-                            }} key={index} className="w-[150px] shrink-0 duration-500">
-                                <img src={"" +cat.image} alt="" />
-                            </div>
-                        )
-                    }
-                )
-            }
+          {categories.map((cat) => (
+            <div
+              key={cat.id}
+              style={{
+                transform: `translateX(-${slide * 100}%)`,
+              }}
+              className="w-[150px] shrink-0 duration-500"
+            >
+              <img src={cat.image} alt={cat.name} className="w-full h-auto" />
+            </div>
+          ))}
         </div>
       </div>
     </>
