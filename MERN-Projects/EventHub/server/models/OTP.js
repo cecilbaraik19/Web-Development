cbb57218d17acfcaf9957b1f16d0d2a301229c61
CecilBaraik <cecilbaraik19@gmail.com>
2026-08-14 00:04:45 +0530

    Final Commit

--- MERN-Projects/EventHub/server/models/OTP.js
+++ MERN-Projects/EventHub/server/models/OTP.js
@@ -1,23 +1,32 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const otpSchema = new mongoose.Schema({
-    email:{
+    email: {
         type: String,
-        required:true
-    },
-    otp:{
-        type:String,
-        required:true
-    },
-    action:{
-        type: String,
-        enum: ['account_verification','event_booking'],
         required: true
     },
-    createdAt:{
+
+    otp: {
+        type: String,
+        required: true
+    },
+
+    action: {
+        type: String,
+        enum: [
+            "account_verification",
+            "event_booking"
+        ],
+        required: true
+    },
+
+    createdAt: {
         type: Date,
         default: Date.now,
-        expires: 300 // OTP expires after 5 minutes
+        expires: 300
     }
 });
-module.exports = mongoose.model('OTP',otpSchema);
+
+const OTP = mongoose.model("OTP", otpSchema);
+
+export default OTP;
