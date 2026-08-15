ad9bfd345d4b4f771fd5d5464ca0ff1574c5ebc6
CecilBaraik <cecilbaraik19@gmail.com>
2026-08-15 09:32:07 +0530

    Basic python code

---
+++ Python/Basic-Python/3_Details.py
@@ -0,0 +1,12 @@
+name = "Cecil Baraik"
+roll = 'A35404925006'
+cgpa = 8.9
+marks = 600
+grade = 'A'
+fee = 64000.00
+print("Name     :",name)
+print("Roll     :",roll)
+print("CGPA     :",cgpa)
+print("Marks    :",marks)
+print("Grade    :",grade)
+print("Fee      :",fee)
