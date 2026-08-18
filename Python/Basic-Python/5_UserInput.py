2e48da3bae937c913812ec71b3fbc5217b5c7f97
CecilBaraik <cecilbaraik19@gmail.com>
2026-08-18 23:44:31 +0530

    basic python

---
+++ Python/Basic-Python/5_UserInput.py
@@ -0,0 +1,19 @@
+print("Enter the value of a & b")
+a = int(input())
+b = int(input())
+print("Value of a :",a)
+print("Value of b :",b)
+c = a+b
+print("Sum of two number:",c)
+c = a-b
+print("Sub of two number:",c)
+c = a*b
+print("Pro of two number:",c)
+c = a/b
+print("Div of two number:",c)
+c = a//b
+print("// Div of two number:",c)
+c = a%b
+print("Mod of two number:",c)
+c = a**b 
+print("Exponential value:",c)
