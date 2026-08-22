630aeef985bb05ec59d939c4de77bc76f43152bc
CecilBaraik <cecilbaraik19@gmail.com>
2026-08-22 13:38:51 +0530

    types of Operators

---
+++ Python/Basic-Python/Operators/BitwiseOp.py
@@ -0,0 +1,9 @@
+a = 10
+b = 15
+print("Value of a       :",a)
+print("Value of b       :",b)
+print("Value of a&b     :",(a&b))
+print("Value of a|b     :",(a|b))
+print("Value of a^b     :",(a^b))
+print("Value of a<<2    :",(a<<2))
+print("Value of a>>2    :",(a>>2))
---
+++ Python/Basic-Python/Operators/LogicalOp.py
@@ -0,0 +1,10 @@
+a = True
+b = False
+print("Value of a           :",a)
+print("Value of b           :",b)
+print("Value of a and a     :",(a and a))
+print("Value of a and b     :",(a and b))
+print("Value of a or b      :",(a or b))
+print("Value of b||b        :",(b or b))
+print("Value of not a       :",(not a))
+print("Value of not b       :",(not b))
