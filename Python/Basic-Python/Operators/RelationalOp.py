ae4fa7c668f559a9734cdc84a826dd33ad8aac06
CecilBaraik <cecilbaraik19@gmail.com>
2026-08-22 13:38:51 +0530

    types of Operators

---
+++ Python/Basic-Python/Operators/RelationalOp.py
@@ -0,0 +1,10 @@
+a = 10
+b = 15
+print("Value of a           :",a)
+print("Value of b           :",b)
+print("Value of a==b        :",(a==b))
+print("Value of a<=b        :",(a<=b))
+print("Value of a>=b        :",(a>=b))
+print("Value of a<b         :",(a<b))
+print("Value of a>b         :",(a>b))
+print("Value of a!=b        :",(a!=b))
