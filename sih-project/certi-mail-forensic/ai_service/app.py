b35f81a57799f6054ef26177a399110914ef119a
CecilBaraik <cecilbaraik19@gmail.com>
2026-09-06 12:22:38 +0530

    code modified in ai_service folder for deploying in render

--- sih-project/certi-mail-forensic/ai_service/app.py
+++ sih-project/certi-mail-forensic/ai_service/app.py
@@ -189,5 +189,8 @@ analyzer = EmailForensicAnalyzer()
 async def analyze_email(payload: EmailPayload):
     return analyzer.analyze(payload.raw_text)
 
+import os
+
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
