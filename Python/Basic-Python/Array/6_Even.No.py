ec3c4dcfba711565ea8b1207ecbb9640088277fd
CecilBaraik <cecilbaraik19@gmail.com>
2026-09-14 18:09:45 +0530

    array basic code

---
+++ Python/Basic-Python/Array/6_Even.No.py
@@ -0,0 +1,10 @@
+from array import*
+arr = array('i',[])
+print('Enter Ten Array Number')
+for i in range(0,10):
+    print('arr[',i,']= ',end='')
+    n = int(input())
+    arr.append(n)
+print('Even number')
+for i in range(1,10,2):
+    print(arr[i])
